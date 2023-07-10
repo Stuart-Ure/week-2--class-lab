ef916cf8707a6a405b321ecef7bcdf1be50eb0f6
Stuart <stuartrure@gmail.com>
2023-07-10 16:41:46 +0100

    lab finished plus extension

--- team_runner.py
+++ team_runner.py
@@ -6,4 +6,17 @@ team = Team ("All stars", players, "Arsene Wenger")
 
 print(team.has_player("Andy Murray")) 
 
+print (team.has_player("Thierry Henry"))
 
+team.add_player(" Zlatan Ibrahimovic") 
+
+print (players)
+
+team.play_game(True)
+print (team.points)
+
+team.play_game(False)
+print (team.points)
+
+team.play_game(True)
+print (team.points)
